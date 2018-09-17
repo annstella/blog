1f70f401be05be329d475c4a506ed5899e552d4a
Annstella <annstellawangui@gmail.com>
2018-09-17 11:40:10 +0300

    added form for commenting and posting

---
+++ app/main/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField
+from wtforms.validators import Required
+
+class CommentForm(FlaskForm):
+    '''
+    Class to create a wtf form for creating a feedback on a post
+    '''
+    comment_content =  TextAreaField('Comment', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class PostForm(FlaskForm):
+    '''
+    Class to create a wtf form for creating a post
+    '''
+    post_title = StringField('Post Title')
+    post_content = TextAreaField('Post Content')
+    submit = SubmitField('Submit')
